e7a596a73f386286404ba57193e36cfc1bcc8631
Alakbarbh <alakbarbh@code.edu.az>
2023-05-17 20:28:21 +0400

    Api-Crud

---
+++ P135-Api-Onion/Services/Mappings/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Services.DTOs.Country;
+using Services.DTOs.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            //Employee
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+            //CreateMap<IEnumerable<Employee>, EmployeeDto>().ReverseMap();
+            CreateMap<EmployeeCreateDto, Employee>();
+            CreateMap<EmployeeUpdateDto, Employee>().ReverseMap();
+            //Country
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<CountryCreateDto, Country>();
+            CreateMap<CountryUpdateDto, Country>().ReverseMap();
+        }
+    }
+}
